c39375030b9c04c8705de1f858a96f567c017ad4
vishu jadhav <vjadhav1822@gmail.com>
2025-09-25 03:04:06 -0700

    Added exceptions and validations in Product service

---
+++ product-service/product-service/src/main/java/com/EcomerceApp/product_service/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.EcomerceApp.product_service.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String msg){
+        super(msg);
+    }
+}
--- product-service/product-service/src/main/java/com/EcomerceApp/product_service/model/Product.java
+++ product-service/product-service/src/main/java/com/EcomerceApp/product_service/model/Product.java
@@ -5,11 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Objects;
 
 @Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
